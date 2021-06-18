7a1321801bd41992959424aab49c0c72a87057e1
gkdud583 <60775067+gkdud583@users.noreply.github.com>
2021-06-18 16:45:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const menu = document.querySelector(".slide-menu");
 
 
 function handleClick(){
-    if(window.getComputedStyle(menu).left === "-400px"){
+    if(window.getComputedStyle(menu).left === "-800px"){
         menu.style.left = 0;
         hamburger.children[0].classList.add("first-span");
         hamburger.children[1].classList.add("second-span");
@@ -13,7 +13,7 @@ function handleClick(){
        
     }
     else{
-        menu.style.left = "-400px";
+        menu.style.left = "-800px";
         hamburger.children[0].classList.remove("first-span");
         hamburger.children[1].classList.remove("second-span");
         hamburger.children[2].classList.remove("third-span");
